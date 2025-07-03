2ec167ec07c59d13bc1bc91a9b847bafbf1fdcc2
Hosheyah Yisrael <139833671+Mcluster369@users.noreply.github.com>
2025-07-03 09:20:16 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// Student information variables
+let studentName = "Hosheyah Yisrael";
+let studentMajor = "Advanced Computer Science";
+let studentEmail = "HOSYISRA@uat.edu"; // updated email
+let studentGradDate = "May 2026"; // updated graduation date
+
+// Display student information in HTML elements
+document.getElementById("name").textContent = "Name: " + studentName;
+document.getElementById("major").textContent = "Major: " + studentMajor;
+document.getElementById("email").textContent = "Email: " + studentEmail;
+document.getElementById("gradDate").textContent = "Expected Graduation: " + studentGradDate;
